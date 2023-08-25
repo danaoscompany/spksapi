beaf93b5edb16c505598fa69a711ce71c8ac6f02
danaoscompany@gmail.com <danaoscompany@gmail.com>
2023-08-26 08:23:02 +0900

    a

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -10,11 +10,15 @@ class User extends CI_Controller {
   public function get_details() {
     $cowID = intval($this->input->post('id'));
       $details = $this->db->query("SELECT * FROM `details` WHERE `cow_id`=".$cowID)->result_array();
+      $details['cow'] = $this->db->query("SELECT * FROM `cows` WHERE `id`=".$cowID)->row_array();
       echo json_encode($details);
     }
     
   public function get_all_details() {
         $details = $this->db->query("SELECT * FROM `details`")->result_array();
+        for ($i=0; $i<sizeof($details); $i++) {
+          $details[$i]['cow'] = $this->db->query("SELECT * FROM `cows` WHERE `id`=".$details[$i]['cow_id'])->row_array();
+        }
         echo json_encode($details);
       }
 }
